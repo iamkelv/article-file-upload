305cffbcd6f8e61be8a040d97a8e190b7b21fad6
iamkelv <moseskelvin4u@gmail.com>
2023-06-11 15:46:12 +0100

    init

---
+++ upload.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const multer = require('multer')
+
+const uploadDirectory = './uploads';
+
+// Set up storage for uploaded files
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+       
+            cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + '-' + file.originalname);
+    }
+  });
+
+  // Create the multer instance
+const upload = multer({ storage: storage });
+
+module.exports = upload;
